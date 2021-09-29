a15849b6949b15c43f0b81b0851b8332e35e8dfc
eric <eric.thomas@formation-industries-fc.fr>
2021-09-29 15:57:02 +0200

    ex2

--- main.c
+++ main.c
@@ -5,6 +5,9 @@ int main() {
     printf("%s","Alexandra Abramof\n"); //marche sans %s
     printf("%s","14 juillet 1975\n");
     printf("%s","0669674556\n");
-
+    /***********************Exercice n°2********************************/
+    int longueur=20,largeur=10;
+    printf("Périmètre du rectangle=%d cm\n",(longueur+largeur)*2);
+    printf("Aire du rectangle=%d cm carrés\n",longueur*largeur);
     return 0;
 }
