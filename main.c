b004e4420e494831c093971edc6dc36b0cac7bb7
eric <eric.thomas@formation-industries-fc.fr>
2021-09-29 15:43:08 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    /************************Exercice n°1******************************/
+    printf("%s","Alexandra Abramof\n"); //marche sans %s
+    printf("%s","14 juillet 1975\n");
+    printf("%s","0669674556\n");
+
+    return 0;
+}
