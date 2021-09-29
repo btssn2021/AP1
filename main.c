1f53a764f026e3a2a3b309bbaf55a7923f67dad1
eric <eric.thomas@formation-industries-fc.fr>
2021-09-29 16:49:02 +0200

    ex6

--- main.c
+++ main.c
@@ -17,6 +17,13 @@ int main() {
     scanf("%f",&rayon);
     printf("Périmètre du cercle %0.6f cm\n",2*PI*rayon);
     printf("Aire du cercle %0.6f cm carrés\n",PI*rayon*rayon);
+    /*********************Exercice  n°4 et 6************************************/
+    int n1=0,n2=0;
+    printf("Entrer le premier nombre entier:");
+    scanf("%d",&n1);
+    printf("Entrer le deuxième nombre entier:");
+    scanf("%d",&n2);
+    printf("Somme des deux nombres entiers ci dessus=%d",n1*n2);
 
     return 0;
 }
